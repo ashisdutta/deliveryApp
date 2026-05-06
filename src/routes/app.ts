8626f2f6a86f7011b74409693a6502b1f7c60bb0
ashisdutta <ashisdutta795@gmail.com>
2026-05-06 21:39:54 +0530

    modified app.ts

--- src/routes/app.ts
+++ src/routes/app.ts
@@ -3,6 +3,7 @@ import authRouter from "./authRoutes.js";
 import userRouter from "./userRoutes.js";
 import resturentRouter from "./restaurantRoutes.js";
 import orderRouter from "./orderRoutes.js";
+import deliverySlabRouter from "./deliveryPriceSlabRoutes.js"
 
 const router = express.Router();
 
@@ -10,5 +11,7 @@ router.use("/auth", authRouter);
 router.use("/user", userRouter);
 router.use("/restaurant", resturentRouter);
 router.use("/order", orderRouter);
+router.use("/slab", deliverySlabRouter)
+
 
 export default router;
