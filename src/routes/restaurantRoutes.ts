31053fceb71cc548fa1403583dcfce64b08e90f7
tufail2005 <tufail1526@gmail.com>
2026-05-08 19:58:52 +0530

    added restaurant owner related routes

--- src/routes/restaurantRoutes.ts
+++ src/routes/restaurantRoutes.ts
@@ -1,20 +1,49 @@
 import { Router } from "express";
 import {
   addRestaurant,
-  //addCatagories,
   addItem,
   itemList,
   deleteItem,
-  //deleteCatagory,
+  toggleRestaurantStatus,
+  updateItem,
 } from "../controllers/restaurantController.js";
 
-import { protect } from "../middlewares/authMiddleware.js";
+import { protect, restrictTo } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.post("/add-restaurant", protect, addRestaurant);
-router.post("/add-item", protect, addItem);
-router.delete("/delete-item/:id", protect, deleteItem);
+router.post(
+  "/add-restaurant",
+  protect,
+  restrictTo("RESTAURANT_OWNER", "SUPER_ADMIN"),
+  addRestaurant
+);
+router.post(
+  "/add-item",
+  protect,
+  restrictTo("RESTAURANT_OWNER", "SUPER_ADMIN"),
+  addItem
+);
+router.delete(
+  "/delete-item/:id",
+  protect,
+  restrictTo("RESTAURANT_OWNER", "SUPER_ADMIN"),
+  deleteItem
+);
+
+router.patch(
+  "/:id/toggle-status",
+  protect,
+  restrictTo("RESTAURANT_OWNER", "SUPER_ADMIN"),
+  toggleRestaurantStatus
+);
+
+router.patch(
+  "/item/:id",
+  protect,
+  restrictTo("RESTAURANT_OWNER", "SUPER_ADMIN"),
+  updateItem
+);
 
 router.get("/:id/items", protect, itemList);
 
