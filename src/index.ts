6d7937d09b60ae10bf6845fb5a17fa9c6bf4f699
tufail2005 <tufail1526@gmail.com>
2026-05-09 22:45:48 +0530

    update cors

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import express, { type Express } from "express";
 import dotenv from "dotenv";
 import mainRouter from "./routes/app.js";
-import cors from 'cors';
+import cors from "cors";
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use("/api", mainRouter);
 
 export { app };
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+
+app.listen(Number(PORT), "0.0.0.0", () => {
+  console.log(`Server running on http://192.168.1.12:${PORT}`);
 });
